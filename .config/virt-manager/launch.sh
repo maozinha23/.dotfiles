573e19f9cf3d807850834159bd6968015d8e5ed1
maozinha23 <20055270+maozinha23@users.noreply.github.com>
2025-05-01 14:11:45 -0300

    Envio inicial

---
+++ .config/virt-manager/launch.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Inicia serviço se ainda não estiver ativo
+start_service() {
+    local service="$1"
+    if systemctl is-active --quiet "${service}"; then
+        echo "Serviço '${service}' já está ativo"
+    else
+        echo "Iniciando serviço '${service}' ..."
+        sudo systemctl start "${service}"
+        if systemctl is-active --quiet "${service}"; then
+            echo "Serviço '${service}' iniciado com sucesso"
+        else
+            echo "Falha ao iniciar o serviço '${service}'"
+            exit 1
+        fi
+    fi
+}
+
+echo "Verificando serviços necessários ..."
+
+start_service libvirtd
+start_service smb
+
+echo "Abrindo virt-manager ..."
+virt-manager &
