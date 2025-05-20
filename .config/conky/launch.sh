a82a399c5efb26ebca7655dcce3a8e880cfe79a6
maozinha23 <20055270+maozinha23@users.noreply.github.com>
2025-05-20 05:40:33 -0300

    Alterado: ordem de execução das duas instâncias do conky. Shebang do "bash" para "sh"

--- .config/conky/launch.sh
+++ .config/conky/launch.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/bash
+#!/bin/sh
 
 # Terminate already running conky instances
 pkill conky
 
-# Launch two instances of conky, OS and System
-conky --config=$HOME/.config/conky/conky-os.conf &
+# Launch two instances of conky, System and OS
 conky --config=$HOME/.config/conky/conky-system.conf &
+conky --config=$HOME/.config/conky/conky-os.conf &
