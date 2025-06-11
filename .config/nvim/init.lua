d3ca429cb5e92a8ebe2c347a7f05f075d926f64e
maozinha23 <20055270+maozinha23@users.noreply.github.com>
2025-06-11 13:00:40 -0300

    Adicionado: require("keymaps") e mapeamento da tecla Leader. Removido: highlights

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,6 @@
---vim.cmd("filetype plugin indent on")
---vim.cmd("syntax enable")
---vim.cmd("colorscheme maozinha23/init")
 require("settings")
+require("keymaps")
 require("colors.maozinha23")
 require("neovide")
---##############################################################################
--- Highlight
---##############################################################################
-vim.cmd("highlight TrailingWhiteSpace ctermbg=darkred")
-vim.cmd("match TrailingWhiteSpace /\\s\\+$/")
+
+vim.g.mapleader = "\\"
