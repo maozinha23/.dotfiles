70ee771be059402bff9a7370d26476169b9e6a73
maozinha23 <20055270+maozinha23@users.noreply.github.com>
2025-06-11 13:03:35 -0300

    Adicionado: mapeamento para a tecla Leader e funcionalidades para listar e alternar buffers, exibir netrw, sair e recarregar configuração

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,1 +1,14 @@
-vim.keymap.set("n", "<Leader>r", "<cmd>source $MYVIMRC<CR>", { noremap = true })
+vim.g.mapleader = " "
+
+vim.keymap.set("n", "<Leader>r", function()
+  require("utils.reload").reload_config()
+end, { noremap = true })
+
+vim.keymap.set("n", "<C-Tab>", "<cmd>bnext<CR>", { noremap = true })
+vim.keymap.set("n", "<C-S-Tab>", "<cmd>bprev<CR>", { noremap = true })
+
+vim.keymap.set("n", "<Leader>l", "<cmd>ls<CR>", { noremap = true })
+
+vim.keymap.set("n", "<Leader>v", "<cmd>Vexplore<CR>", { noremap = true })
+
+vim.keymap.set("n", "<Leader>q", "<cmd>quit<CR>", { noremap = true })
