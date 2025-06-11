d5baf0d5e562cdc203e01f787319d7e309297bbc
maozinha23 <20055270+maozinha23@users.noreply.github.com>
2025-06-11 13:11:02 -0300

    Adicionado: "scale_factor". Alterado: fonte de "Cousine" para "Terminess"

--- .config/nvim/lua/neovide.lua
+++ .config/nvim/lua/neovide.lua
@@ -1,6 +1,7 @@
 if vim.g.neovide then
-  vim.g.neovide_cursor_vfx_mode = ""
   vim.g.neovide_cursor_animation_length = 0
   vim.g.neovide_cursor_trail_length = 0
-  vim.o.guifont = "Cousine Nerd Font:h12"
+  vim.g.neovide_cursor_vfx_mode = ""
+  vim.g.neovide_scale_factor = 1.0
+  vim.o.guifont = "Terminess Nerd Font:h13"
 end
