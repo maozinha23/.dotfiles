29fbb30852270be89449f34c5794dd6674559ca5
maozinha23 <20055270+maozinha23@users.noreply.github.com>
2025-06-14 04:49:40 -0300

    Alterado: algumas aspas duplas para simples e outras pequenas formatações do arquivo

--- .config/virt-manager/launch.sh
+++ .config/virt-manager/launch.sh
@@ -3,24 +3,26 @@
 # Inicia serviço se ainda não estiver ativo
 start_service() {
     local service="$1"
+
     if systemctl is-active --quiet "${service}"; then
-        echo "Serviço '${service}' já está ativo"
+        echo "Serviço '${service}' já está ativo."
     else
         echo "Iniciando serviço '${service}' ..."
         sudo systemctl start "${service}"
+
         if systemctl is-active --quiet "${service}"; then
-            echo "Serviço '${service}' iniciado com sucesso"
+            echo "Serviço '${service}' iniciado com sucesso."
         else
-            echo "Falha ao iniciar o serviço '${service}'"
+            echo "Falha ao iniciar o serviço '${service}'."
             exit 1
         fi
     fi
 }
 
-echo "Verificando serviços necessários ..."
+echo 'Verificando serviços necessários ...'
 
 start_service libvirtd
 start_service smb
 
-echo "Abrindo virt-manager ..."
+echo 'Abrindo virt-manager ...'
 command virt-manager &
