e34a9ea69614e8a70d8bd4f44f65afbbb719ad80
maozinha23 <20055270+maozinha23@users.noreply.github.com>
2025-06-21 06:54:48 -0300

    Alterado: formatação do arquivo e remoção de "local"

--- .config/virt-manager/launch.sh
+++ .config/virt-manager/launch.sh
@@ -2,21 +2,21 @@
 
 # Inicia serviço se ainda não estiver ativo
 start_service() {
-    local service="$1"
+  service="$1"
+
+  if systemctl is-active --quiet "${service}"; then
+    echo "Serviço '${service}' já está ativo."
+  else
+    echo "Iniciando serviço '${service}' ..."
+    sudo systemctl start "${service}"
 
     if systemctl is-active --quiet "${service}"; then
-        echo "Serviço '${service}' já está ativo."
+        echo "Serviço '${service}' iniciado com sucesso."
     else
-        echo "Iniciando serviço '${service}' ..."
-        sudo systemctl start "${service}"
-
-        if systemctl is-active --quiet "${service}"; then
-            echo "Serviço '${service}' iniciado com sucesso."
-        else
-            echo "Falha ao iniciar o serviço '${service}'."
-            exit 1
-        fi
+        echo "Falha ao iniciar o serviço '${service}'."
+        exit 1
     fi
+  fi
 }
 
 echo 'Verificando serviços necessários ...'
