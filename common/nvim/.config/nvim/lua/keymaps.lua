1337d2e4491805d415013d7efaca2c00ab0943b9
maozinha23 <20055270+maozinha23@users.noreply.github.com>
2025-06-25 18:59:00 -0300

    Adicionado: constante TOGGLE_BACKGROUND. Alterado: WALLPAPER para FEH_WALLPAPER

--- common/nvim/.config/nvim/lua/keymaps.lua
+++ common/nvim/.config/nvim/lua/keymaps.lua
@@ -1,14 +1,24 @@
+-- Set Leader key
 vim.g.mapleader = " "
 
+-- Reload configuration
 vim.keymap.set("n", "<Leader>r", function()
   require("utils.reload").reload_config()
 end, { noremap = true })
 
+-- Cycle through open buffers
 vim.keymap.set("n", "<C-Tab>", "<cmd>bnext<CR>", { noremap = true })
 vim.keymap.set("n", "<C-S-Tab>", "<cmd>bprev<CR>", { noremap = true })
 
+-- List open buffers
 vim.keymap.set("n", "<Leader>l", "<cmd>ls<CR>", { noremap = true })
 
+-- Open Netrw in a vertical split
 vim.keymap.set("n", "<Leader>v", "<cmd>Vexplore<CR>", { noremap = true })
 
+-- Move line up/down
+vim.keymap.set("n", "<Leader><Up>", "<cmd>move -2<CR>", { noremap = true })
+vim.keymap.set("n", "<Leader><Down>", "<cmd>move +1<CR>", { noremap = true })
+
+-- Quit
 vim.keymap.set("n", "<Leader>q", "<cmd>quit<CR>", { noremap = true })
