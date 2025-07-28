cb263be7fcf5fcf95416c95523561e0401314f6f
maozinha23 <20055270+maozinha23@users.noreply.github.com>
2025-07-28 13:18:20 -0300

    Ícones alterados para nomes da Freedesktop.org

--- common/rofi/.config/rofi/scripts/power.sh
+++ common/rofi/.config/rofi/scripts/power.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
-# CONSTANTS {{{
-COLOR_ICON="white"
-ICON_QUIT="󰩈"
-ICON_REBOOT="󰜉"
-ICON_SHUTDOWN="󰐥"
+# CONSTANTS
+ICON_QUIT="system-log-out"
+ICON_REBOOT="system-reboot"
+ICON_SHUTDOWN="system-shutdown"
 TEXT_QUIT="Quit"
 TEXT_REBOOT="Reboot"
 TEXT_SHUTDOWN="Shutdown"
-# }}}
 
-# SCRIPT {{{
+# SCRIPT
 option="$1"
 
 case "${option}" in
@@ -19,12 +17,8 @@ case "${option}" in
   "${TEXT_SHUTDOWN}") systemctl poweroff ;;
 esac
 
-# rofi entry with a markup string icon using pango
-# "Entry name\0icon\x1f<span color='color_name'>string_icon</span>\n"
-printf "%s\0icon\x1f<span color='${COLOR_ICON}'>%s</span>\n" \
-  "${TEXT_QUIT}" "${ICON_QUIT}"
-printf "%s\0icon\x1f<span color='${COLOR_ICON}'>%s</span>\n" \
-  "${TEXT_REBOOT}" "${ICON_REBOOT}"
-printf "%s\0icon\x1f<span color='${COLOR_ICON}'>%s</span>\n" \
-  "${TEXT_SHUTDOWN}" "${ICON_SHUTDOWN}"
-# }}}
+# rofi entry with icon-name, this is looked up via the icon-theme
+# "Entry name\0icon\x1fFREEDESKTOP_ICON_NAME\n"
+printf "%s\0icon\x1f%s\n" "${TEXT_QUIT}" "${ICON_QUIT}"
+printf "%s\0icon\x1f%s\n" "${TEXT_REBOOT}" "${ICON_REBOOT}"
+printf "%s\0icon\x1f%s\n" "${TEXT_SHUTDOWN}" "${ICON_SHUTDOWN}"
