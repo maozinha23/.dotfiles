9b5168e15ca8124b4b7a3249a86788e8607f4b48
maozinha23 <20055270+maozinha23@users.noreply.github.com>
2025-09-03 17:26:11 -0300

    Altera o idioma para inglês (C.UTF-8)

--- common/nvim/.config/nvim/init.lua
+++ common/nvim/.config/nvim/init.lua
@@ -1,5 +1,7 @@
-require("config.settings")
+require("config.options")
 require("config.keymaps")
 require("config.neovide")
-require("config.lazy")
 require("colors.maozinha23")
+require("config.lazy")
+
+vim.cmd("language C.UTF-8")
