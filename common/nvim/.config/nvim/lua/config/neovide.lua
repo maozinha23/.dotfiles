95d045a86df4641fedad69734f6a6066c7d69acf
maozinha23 <20055270+maozinha23@users.noreply.github.com>
2025-09-03 20:10:40 -0300

    Alterado o tamanho da fonte

--- common/nvim/.config/nvim/lua/config/neovide.lua
+++ common/nvim/.config/nvim/lua/config/neovide.lua
@@ -1,7 +1,7 @@
 if vim.g.neovide then
+  vim.o.guifont = "Terminess Nerd Font:h10"
   vim.g.neovide_cursor_animation_length = 0
   vim.g.neovide_cursor_trail_length = 0
   vim.g.neovide_cursor_vfx_mode = ""
   vim.g.neovide_scale_factor = 1.0
-  vim.o.guifont = "Terminess Nerd Font:h13"
 end
