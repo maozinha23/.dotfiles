2fd374e412de92e8f7e0a93178907ce7ea8563ec
maozinha23 <20055270+maozinha23@users.noreply.github.com>
2025-09-18 19:59:23 -0300

    Envio inicial

---
+++ common/firefox/.mozilla/firefox/default/user-overrides.js
@@ -0,0 +1,72 @@
+//------------------------------------------------------------------------------
+// api
+//------------------------------------------------------------------------------
+user_pref("geo.enabled", false);
+user_pref("full-screen-api.enabled", false);
+//------------------------------------------------------------------------------
+// browser
+//------------------------------------------------------------------------------
+user_pref("browser.formfill.enable", false);
+user_pref("browser.translations.automaticallyPopup", false);
+user_pref("browser.urlbar.clipboard.featureGate", false);
+user_pref("browser.urlbar.recentsearches.featureGate", false);
+user_pref("browser.urlbar.suggest.addons", false);
+user_pref("browser.urlbar.suggest.calculator", false);
+user_pref("browser.urlbar.suggest.clipboard", false);
+user_pref("browser.urlbar.suggest.engines", false);
+user_pref("browser.urlbar.suggest.openpage", false);
+user_pref("browser.urlbar.suggest.quickactions", false);
+user_pref("browser.urlbar.suggest.recentsearches", false);
+user_pref("browser.urlbar.suggest.topsites", false);
+user_pref("browser.urlbar.suggest.trending", false);
+user_pref("browser.urlbar.suggest.weather", false);
+//------------------------------------------------------------------------------
+// dom
+//------------------------------------------------------------------------------
+user_pref("dom.push.enabled", false);
+user_pref("dom.webnotifications.enabled", false);
+//------------------------------------------------------------------------------
+// extensions
+//------------------------------------------------------------------------------
+user_pref("extensions.formautofill.addresses.enabled", false);
+user_pref("extensions.formautofill.creditCards.enabled", false);
+//------------------------------------------------------------------------------
+// media
+//------------------------------------------------------------------------------
+user_pref("media.eme.enabled", true);
+//------------------------------------------------------------------------------
+// permissions
+//------------------------------------------------------------------------------
+user_pref("permissions.default.geo", 2);
+user_pref("permissions.default.camera", 2);
+user_pref("permissions.default.microphone", 2);
+user_pref("permissions.default.desktop-notification", 2);
+user_pref("permissions.default.xr", 2);
+//------------------------------------------------------------------------------
+// privacy
+//------------------------------------------------------------------------------
+user_pref("privacy.clearHistory.browsingHistoryAndDownloads", true);
+user_pref("privacy.clearHistory.cache", true);
+user_pref("privacy.clearHistory.cookiesAndStorage", true);
+user_pref("privacy.clearHistory.formdata", true);
+user_pref("privacy.clearHistory.historyFormDataAndDownloads", true);
+user_pref("privacy.clearHistory.siteSettings", true);
+user_pref("privacy.clearSiteData.browsingHistoryAndDownloads", true);
+user_pref("privacy.clearSiteData.cache", true);
+user_pref("privacy.clearSiteData.cookiesAndStorage", true);
+user_pref("privacy.clearSiteData.formdata", true);
+user_pref("privacy.clearSiteData.historyFormDataAndDownloads", true);
+user_pref("privacy.clearSiteData.siteSettings", true);
+user_pref("privacy.sanitize.sanitizeOnShutdown", false);
+//------------------------------------------------------------------------------
+// sidebar
+//------------------------------------------------------------------------------
+user_pref("sidebar.animation.enabled", false);
+user_pref("sidebar.main.tools", "history,bookmarks");
+user_pref("sidebar.revamp", true);
+user_pref("sidebar.verticalTabs", true);
+//------------------------------------------------------------------------------
+// signon
+//------------------------------------------------------------------------------
+user_pref("signon.management.page.vulnerable-passwords.enabled", false);
+user_pref("signon.rememberSignons", false);
